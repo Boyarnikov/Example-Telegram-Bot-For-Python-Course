fcac125f9e451ebc488f419b61db28811e7fe097
Boyar <boiarnikov.ia@phystech.edu>
2024-03-15 21:50:21 +0300

    Add bot init

---
+++ BotSetup.py
@@ -0,0 +1,20 @@
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, InlineQueryHandler, PicklePersistence
+from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
+from secrets import TOKEN
+import Model
+
+
+def setup_app():
+    per = PicklePersistence(filepath="data")
+
+    app_b = ApplicationBuilder()
+    app_b.token(TOKEN)
+    app_b.persistence(per)
+    app = app_b.build()
+
+    app.bot_data["lists"] = Model.DataList.lists
+    app.bot_data["users"] = Model.User.users
+    app.bot_data["keys"] = Model.Key.keys
+
+    return app
+
